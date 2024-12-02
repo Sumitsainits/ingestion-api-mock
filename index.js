1bff05905a467aa66ea2e269100c5add4fe5db24
Sumit Saini <saini.sumit@thinksys.com>
2024-12-02 14:59:39 +0530

    updated code to mark ingestion process as completed

--- index.js
+++ index.js
@@ -8,12 +8,25 @@ app.use(express.json());
 
 const ingestionProcesses = {};
 
+const markProcessAsCompleted = (processId) => {
+  const randomInterval = Math.floor(Math.random() * 10000) + 1000;
+
+  setTimeout(() => {
+    if (ingestionProcesses[processId]) {
+      ingestionProcesses[processId].status = "Completed";
+      ingestionProcesses[processId].completedAt = new Date();
+    }
+  }, randomInterval);
+};
+
 app.post("/ingest", (req, res) => {
   const processId = uuidv4();
   ingestionProcesses[processId] = {
     status: "In Progress",
     timestamp: new Date(),
   };
+
+  markProcessAsCompleted();
 
   res.json({
     message: "Ingestion process started",
@@ -29,6 +42,7 @@ app.get("/ingest/status/:processId", (req, res) => {
       processId,
       status: ingestionProcesses[processId].status,
       startedAt: ingestionProcesses[processId].timestamp,
+      completedAt: ingestionProcesses[processId].completedAt || null,
     });
   } else {
     res.status(404).json({
